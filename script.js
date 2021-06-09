6ca2a6b9d37662d2bb7183aa04712dd26db124b7
MakrMohamed <84314376+MakrMohamed@users.noreply.github.com>
2021-06-09 13:10:44 +0200

    Add files via upload
    Tweede Javascript Opdracht

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hallo Winc Academy studenten");
+
+/* This is
+
+//Dit is een grote som
+let sum = 1230941 * 1823794;
+console.log("sum:", sum);
+
+let sum2 = 637263 / 54;
+console.log("sum2:", sum2);
+
+a comment */
